8e9130378b5925f31b07f1c0f7b9d843936b701b
egonl <egon.lubbers@gmail.com>
2021-10-15 23:11:38 +0200

    Reformatted code.

--- SharpDocx/ImageHelper.cs
+++ SharpDocx/ImageHelper.cs
@@ -16,9 +16,9 @@ namespace SharpDocx
     public static class ImageHelper
     {
         public static Drawing CreateDrawing(
-            WordprocessingDocument package, 
+            WordprocessingDocument package,
             Stream imageStream,
-            ImagePartType imagePartType, 
+            ImagePartType imagePartType,
             int percentage, long maxWidthInEmus)
         {
             var imagePart = package.MainDocumentPart.AddImagePart(imagePartType);
@@ -63,14 +63,14 @@ namespace SharpDocx
             var vertRezDpi = imageInfo.DpiV;
 #endif
             const int emusPerInch = 914400;
-            var widthEmus = (long) (widthPx / horzRezDpi * emusPerInch);
-            var heightEmus = (long) (heightPx / vertRezDpi * emusPerInch);
+            var widthEmus = (long)(widthPx / horzRezDpi * emusPerInch);
+            var heightEmus = (long)(heightPx / vertRezDpi * emusPerInch);
 
             if (widthEmus > maxWidthInEmus)
             {
                 var ratio = heightEmus * 1.0m / widthEmus;
                 widthEmus = maxWidthInEmus;
-                heightEmus = (long) (widthEmus * ratio);
+                heightEmus = (long)(widthEmus * ratio);
             }
 
             if (percentage != 100)
@@ -164,7 +164,7 @@ namespace SharpDocx
         {
             return new Drawing(
                 new DW.Inline(
-                    new DW.Extent {Cx = widthEmus, Cy = heightEmus},
+                    new DW.Extent { Cx = widthEmus, Cy = heightEmus },
                     new DW.EffectExtent
                     {
                         LeftEdge = 0L,
@@ -178,7 +178,7 @@ namespace SharpDocx
                         Name = "Picture 1"
                     },
                     new DW.NonVisualGraphicFrameDrawingProperties(
-                        new A.GraphicFrameLocks {NoChangeAspect = true}),
+                        new A.GraphicFrameLocks { NoChangeAspect = true }),
                     new A.Graphic(
                         new A.GraphicData(
                                 new PIC.Picture(
@@ -192,7 +192,7 @@ namespace SharpDocx
                                     new PIC.BlipFill(
                                         new A.Blip(
                                             new A.BlipExtensionList(
-                                                new A.BlipExtension {Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}"}))
+                                                new A.BlipExtension { Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}" }))
                                         {
                                             Embed = relationshipId,
                                             CompressionState = A.BlipCompressionValues.Print
@@ -201,13 +201,13 @@ namespace SharpDocx
                                             new A.FillRectangle())),
                                     new PIC.ShapeProperties(
                                         new A.Transform2D(
-                                            new A.Offset {X = 0, Y = 0},
-                                            new A.Extents {Cx = widthEmus, Cy = heightEmus}),
+                                            new A.Offset { X = 0, Y = 0 },
+                                            new A.Extents { Cx = widthEmus, Cy = heightEmus }),
                                         new A.PresetGeometry(
                                                 new A.AdjustValueList())
                                             {Preset = A.ShapeTypeValues.Rectangle}))
                             )
-                            {Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture"})
+                            { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
                 )
                 {
                     DistanceFromTop = 0,
