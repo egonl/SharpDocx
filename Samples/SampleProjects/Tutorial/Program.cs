35b81199489cc345c2daf6fccf7f7c86f8d34fe1
egonl <egon.lubbers@gmail.com>
2026-09-24 14:56:54 +0200

    Simplified Tutorial sample.

--- Samples/SampleProjects/Tutorial/Program.cs
+++ Samples/SampleProjects/Tutorial/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using SharpDocx;
-#if NET35 || NET40
-using System.Net;
-#endif
 
 namespace Tutorial
 {
@@ -14,18 +10,13 @@ namespace Tutorial
 
         private static void Main()
         {
-#if NET35 || NET40
-            // The Tutorial fetches an image from https://github.com/egonl/SharpDocx/blob/master/Samples/Images/test3.emf?raw=true,
-            // which requires TLS 1.2. 
-            ServicePointManager.SecurityProtocol |= (SecurityProtocolType)3072;
-#endif
             var viewPath = $"{BasePath}/Views/Tutorial.cs.docx";
             var documentPath = $"{BasePath}/Documents/Tutorial.docx";
             var imageDirectory = $"{BasePath}/Images";
 
 #if DEBUG
-            string documentViewer = null; // .NET Framework 3.5 or greater will automatically search for a Docx viewer.
-            //var documentViewer = @"C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"; // NETCOREAPP3_1 and NET6_0 won't.
+            string documentViewer = null; // Try to find a document viewer automatically.
+            //var documentViewer = @"C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"; // Or specify a document viewer manually. 
 
             Ide.Start(viewPath, documentPath, null, null, f => f.ImageDirectory = imageDirectory, documentViewer);
 #else
